9bfd9e5eafeb774f63b65653ce7a029c142adea3
ashab-k <ashabkhan2000@gmail.com>
2025-04-16 19:27:03 +0530

    fix: fixes error with paddle hitbox

--- internal/game/physics/collison.go
+++ internal/game/physics/collison.go
@@ -5,34 +5,39 @@ import (
 )
 
 func CheckPaddleBallCollision(ball *entity.Ball, paddle *entity.Paddle) bool {
-    // Calculate paddle boundaries
-    paddleLeft := paddle.X - paddle.Width/2
-    paddleRight := paddle.X + paddle.Width/2
-    paddleTop := paddle.Y - paddle.Height/2
-    paddleBottom := paddle.Y + paddle.Height/2
-    
-    if ball.X+ball.Radius >= paddleLeft && 
-       ball.X-ball.Radius <= paddleRight &&
-       ball.Y+ball.Radius >= paddleTop &&
-       ball.Y-ball.Radius <= paddleBottom {
-        
-        handlePaddleBallResponse(ball, paddle)
-        return true
+    paddleLeft := paddle.X
+    paddleRight := paddle.X + paddle.Width
+    paddleTop := paddle.Y
+    paddleBottom := paddle.Y + paddle.Height
+
+    if ball.X+ball.Radius >= paddleLeft &&
+        ball.X-ball.Radius <= paddleRight &&
+        ball.Y+ball.Radius >= paddleTop &&
+        ball.Y-ball.Radius <= paddleBottom {
+
+        if paddle.X < 320/2 && ball.Dx < 0 {
+            handlePaddleBallResponse(ball, paddle)
+            return true
+        } else if paddle.X > 320/2 && ball.Dx > 0 {
+            handlePaddleBallResponse(ball, paddle)
+            return true
+        }
     }
-    
+
     return false
 }
 
+
 func handlePaddleBallResponse(ball *entity.Ball, paddle *entity.Paddle) {
     ball.Dx = -ball.Dx 
-    
-    hitPosition := (ball.Y - paddle.Y) / (paddle.Height / 2)
-    ball.Dy = hitPosition * 2
-    
-   if paddle.X < 160 {
-	ball.X = paddle.X + paddle.Width/2 + ball.Radius + 1
-   }else {
-	ball.X = paddle.X - paddle.Width/2 - ball.Radius - 1;
-   }
-  
+
+    paddleCenterY := paddle.Y + paddle.Height/2
+    hitPosition := (ball.Y - paddleCenterY) / (paddle.Height / 2)
+    ball.Dy = hitPosition * 2 
+
+    if paddle.X < 160 {
+        ball.X = paddle.X + paddle.Width + ball.Radius + 1
+    } else {
+        ball.X = paddle.X - ball.Radius - 1
+    }
 }
